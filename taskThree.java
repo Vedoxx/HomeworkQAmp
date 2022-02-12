76292bb62cc57211a53a44b3441936bbf96901a8
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-12 18:11:58 +0100

    Update taskThree.java

--- taskThree.java
+++ taskThree.java
@@ -10,28 +10,30 @@ public class taskThree {
             if ((A > 10) && (A % 3 == 0) && (A <= 10000)) {
                 System.out.print("Type the value of B:");
                 float B = object.nextFloat();
-                if (A > B) {
-                    float C = A + B;
-                    System.out.println("Result is:" + C);
-                    System.out.println("Would you like to start again (Y-yes)(N-no)?");
-                    System.out.print("Answer:");
-                    answer=object.next().charAt(0);
-                } else if (B > A) {
-                    float C = B - A;
-                    System.out.println("Result is:" + C);
-                    System.out.println("Would you like to start again (Y-yes)(N-no)?");
-                    System.out.print("Answer:");
-                    answer=object.next().charAt(0);
-                } else {
-                    float C = A * B;
-                    System.out.println("Result is:" + C);
+                if(B==0)
+                {
+                    System.out.println("Undefined");
                     System.out.println("Would you like to start again (Y-yes)(N-no)?");
                     System.out.print("Answer:");
                     answer=object.next().charAt(0);
                 }
-            } else {
-                System.out.println("A is not valid!");
-                System.out.println("Would you like to enter A again (Y-yes)(N-no)?");
+                else if (A%B==0){
+                    System.out.println("A is divisible with number B");
+                    System.out.println("Would you like to start again (Y-yes)(N-no)?");
+                    System.out.print("Answer:");
+                    answer=object.next().charAt(0);
+                }
+                else if(A%B!=0)
+                {
+                    System.out.println("A is not divisible with number B");
+                    System.out.println("Would you like to start again (Y-yes)(N-no)?");
+                    System.out.print("Answer:");
+                    answer=object.next().charAt(0);
+                }
+            }
+            else{
+                System.out.println("A is not valid");
+                System.out.println("Would you like to start again (Y-yes)(N-no)?");
                 System.out.print("Answer:");
                 answer=object.next().charAt(0);
             }
