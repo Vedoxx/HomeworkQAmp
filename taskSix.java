0148c20a95b977fa90be58de450a6a9e07075b98
Vedad Karalić <61968590+Vedoxx@users.noreply.github.com>
2022-02-14 12:51:25 +0100

    Update taskSix.java

--- taskSix.java
+++ taskSix.java
@@ -15,35 +15,26 @@ public class taskSix {
             int D = object.nextInt();
              if((A>0)&&(B>0)&&(C>0)&&(D>0)) {
                 System.out.println("The condition did not pass");
-                System.out.println("Would you like to enter A again (Y-yes)(N-no)?");
-                System.out.print("Answer:");
-                answer = object.next().charAt(0);
             }
             else if((A>0 && B>0) || (C>0 && D>0)) {
                 int result = A + B + C + D;
                  if(result<100) {
                      System.out.println("Result is not bigger than 100");
-                     System.out.println("Would you like to enter A again (Y-yes)(N-no)?");
-                     System.out.print("Answer:");
-                     answer = object.next().charAt(0);
                  }
                  else if ((result > 100) && (result % 2 == 0)) {
                     int result2 = result / 2;
                     System.out.println("Result:" + result2);
-                    System.out.println("Would you like to enter A again (Y-yes)(N-no)?");
-                    System.out.print("Answer:");
-                    answer = object.next().charAt(0);
                 } else if (result % 2 != 0) {
                     result = result - 1;
                     if (result % 2 == 0) {
                         result = result / 2;
                         System.out.println("Result:" + result);
-                        System.out.println("Would you like to enter A again (Y-yes)(N-no)?");
-                        System.out.print("Answer:");
-                        answer = object.next().charAt(0);
                     } else System.out.println("The number is still not divisible by 2");
                 }
             }
+            System.out.println("Would you like to enter A again (Y-yes)(N-no)?");
+            System.out.print("Answer:");
+            answer = object.next().charAt(0);
         }while((answer=='y')||(answer=='Y'));
     }
 }
